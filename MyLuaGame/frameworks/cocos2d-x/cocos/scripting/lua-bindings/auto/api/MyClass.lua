30e0113c5ca200a4d60fd8b3a843604091ecf2ac
wang guo dong <jiephe@users.noreply.github.com>
2017-01-18 15:20:27 +0800

    test c++ and lua communication test

---
+++ MyLuaGame/frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/api/MyClass.lua
@@ -0,0 +1,49 @@
+
+--------------------------------
+-- @module MyClass
+-- @extend Ref
+-- @parent_module 
+
+--------------------------------
+-- 
+-- @function [parent=#MyClass] init 
+-- @param self
+-- @return bool#bool ret (return value: bool)
+        
+--------------------------------
+-- 
+-- @function [parent=#MyClass] foo 
+-- @param self
+-- @param #int i
+-- @return int#int ret (return value: int)
+        
+--------------------------------
+-- 
+-- @function [parent=#MyClass] TransmitLuaContructWithCallBack 
+-- @param self
+-- @param #int func
+-- @return int#int ret (return value: int)
+        
+--------------------------------
+-- 
+-- @function [parent=#MyClass] TransmitLuaContruct 
+-- @param self
+-- @param #int nReq
+-- @param #char pData
+-- @param #char pData2
+-- @param #int nDateLen
+-- @return int#int ret (return value: int)
+        
+--------------------------------
+-- 
+-- @function [parent=#MyClass] create 
+-- @param self
+-- @return MyClass#MyClass ret (return value: MyClass)
+        
+--------------------------------
+-- 
+-- @function [parent=#MyClass] MyClass 
+-- @param self
+-- @return MyClass#MyClass self (return value: MyClass)
+        
+return nil
